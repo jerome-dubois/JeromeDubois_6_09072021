ccfec9a2c9bd8ee9ff132b3aa1832876e8bd8df2
jerome-dubois <jerome.dubois@gmail.com>
2021-08-28 16:26:46 +0200

    Fix on middlewares

--- backend/middleware/sauceValidator.js
+++ backend/middleware/sauceValidator.js
@@ -1,11 +1,24 @@
 const validate = require('mongoose-validator');
 
+// module.exports = [validate({
+//     validator: 'isLength',
+//     arguments: [3, 50],
+//     message: 'Le nom de la sauce doit comprendre entre 3 et 50 caractères',
+// }), validate({
+//     validator: 'isAlphanumeric',
+//     arguments: /\s/,
+//     passIfEmpty: true,
+//     message: 'Name should contain alpha-numeric characters only',
+//   })
+// ];
+
 module.exports = [validate({
     validator: 'isLength',
     arguments: [3, 50],
     message: 'Le nom de la sauce doit comprendre entre 3 et 50 caractères',
 }), validate({
-    validator: 'isAlphanumeric',
-    passIfEmpty: true,
-    message: 'Name should contain alpha-numeric characters only',
-  }),];
+    validator: 'matches',
+    arguments: /^[a-z0-9 ]+$/i,
+    message: 'Name should contain alpha-numeric characters only with space allowed',
+  })
+];
